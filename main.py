66d96af7d363ea7833e224b83d475b953108d4d8
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-07-14 13:20:27 +0500

    setting up the project

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+
+load_dotenv()
+
